22dedda0c72fbd30f2cbf908cc9a4c54f7043a47
GioLucero <gioandrewlucero@gmail.com>
2019-10-07 13:58:09 -0400

    Added a row of images that lists the counter of each hero. No data has been added. These rows include a navigation link to the hero image view.

---
+++ Mobile Legends Hero Counter App/Models/CountersRow.swift
@@ -0,0 +1,68 @@
+//
+//  CountersRow.swift
+//  Mobile Legends Hero Counter App
+//
+//  Created by Gio Lucero on 2019-10-07.
+//  Copyright © 2019 Gio Lucero. All rights reserved.
+//
+
+import SwiftUI
+
+struct CountersRow: View {
+    let hero : Hero
+    
+    var body: some View {
+        // HStack held together by the VStack to align it vertically
+        VStack {
+            NavigationLink(destination: HeroImage(hero: hero)) {
+            // Set in an HStack to have the images side by side
+            HStack {
+                // List of counters heroes
+                Image(hero.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 90, height: 180)
+                    .border(Color.white, width: 5)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+
+                Image(hero.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 90, height: 180)
+                    .border(Color.white, width: 5)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                
+                Image(hero.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 90, height: 180)
+                    .border(Color.white, width: 5)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                            
+                Image(hero.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 90, height: 180)
+                    .border(Color.white, width: 5)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+
+    //                .background(Rectangle()
+    //                .fill(Color.green)
+    //                .frame(width: 410, height: 200)
+    //                .offset(x: 0, y: 110))
+                    }
+                }
+            }
+        }
+    }
+
+
+struct CountersRow_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView { CountersRow(hero : testData[0]) }
+    }
+}
